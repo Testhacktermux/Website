87ad43d0ee1b9b9396b5534be93b859e7c3ea2e2
Testhacktermux <bechinh610@gmail.com>
2024-10-25 20:21:16 +0700

    Update Save.py

--- Save.py
+++ Save.py
@@ -11,3 +11,16 @@ users = {
 @app.route('/')
 def home():
     return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    if username in users and users[username] == password:
+        return redirect("https://www.google.com")
+    else:
+        return "Tên đăng nhập hoặc mật khẩu không đúng!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
